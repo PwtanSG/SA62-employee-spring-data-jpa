16c7ee47feadd43b2875dffdadc185cf2e819fbc
pwtansg <pwtan.edu@gmail.com>
2026-03-17 13:30:46 +0800

    Added basic department model

---
+++ src/main/java/sg/edu/nus/spring_data_jpa/model/Department.java
@@ -0,0 +1,59 @@
+package sg.edu.nus.spring_data_jpa.model;
+
+import java.util.Objects;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "departments")
+public class Department {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(nullable = false, length = 60)
+	private String name;
+	
+	public Long getId() {
+		return id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	@Override
+	public String toString() {
+		return "Department [id=" + id + ", name=" + name + "]";
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Department other = (Department) obj;
+		return Objects.equals(id, other.id);
+	}
+	public Department() {
+		super();
+	}
+	public Department(String name) {
+		super();
+		this.name = name;
+	}
+	
+	
+}
