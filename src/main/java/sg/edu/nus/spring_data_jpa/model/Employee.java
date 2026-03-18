05a3b0139c9d8b8351d371d391a6ad9d384c53b5
pwtansg <pwtan.edu@gmail.com>
2026-03-18 08:30:47 +0800

    Update employee and dept models map association

--- src/main/java/sg/edu/nus/spring_data_jpa/model/Employee.java
+++ src/main/java/sg/edu/nus/spring_data_jpa/model/Employee.java
@@ -2,11 +2,14 @@ package sg.edu.nus.spring_data_jpa.model;
 
 import java.util.Objects;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -16,25 +19,39 @@ public class Employee {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	@Column(nullable=false, length = 60)
+	@Column(nullable = false, length = 60)
 	private String name;
+
 	public Long getId() {
 		return id;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
+	public Department getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(Department department) {
+		this.department = department;
+	}
+
 	@Override
 	public String toString() {
 		return "Employee [id=" + id + ", name=" + name + "]";
 	}
+
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
 	}
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -46,13 +63,31 @@ public class Employee {
 		Employee other = (Employee) obj;
 		return Objects.equals(id, other.id);
 	}
+
 	public Employee() {
 		super();
 	}
+
 	public Employee(String name) {
 		super();
 		this.name = name;
+		System.out.println(name);
+	}
+	
+	
+
+	public Employee(String name, Department department) {
+		super();
+		this.name = name;
+		this.department = department;
 	}
 
-	
+
+
+	@OneToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "department_id", referencedColumnName = "id")
+	private Department department;
+//    @OneToOne(cascade = CascadeType.ALL)
+//    @JoinColumn(name = "profile_id", referencedColumnName = "id")
+//    private Profile profile;  // Reference to the Profile entity
 }
